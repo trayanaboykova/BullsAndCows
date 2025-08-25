e77c7a1109826fdd0334a896442b0e1b6c4f81c6
Trayana Boykova <trayana.boykova@gmail.com>
2025-08-25 17:20:34 +0300

    completed first stage

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,28 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.\n");
+
+        System.out.println("Turn 1. Answer:");
+        System.out.println("1234");
+        System.out.println("Grade: 1 cow.\n");
+
+        System.out.println("Turn 2. Answer:");
+        System.out.println("5678");
+        System.out.println("Grade: 1 bull and 1 cow.\n");
+
+        System.out.println("Turn 3. Answer:");
+        System.out.println("9012");
+        System.out.println("Grade: 2 bulls.\n");
+
+        System.out.println("Turn 4. Answer:");
+        System.out.println("9032");
+        System.out.println("Grade: 3 bulls.\n");
+
+        System.out.println("Turn 5. Answer:");
+        System.out.println("9013");
+        System.out.println("Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9013.");
+    }
+}
